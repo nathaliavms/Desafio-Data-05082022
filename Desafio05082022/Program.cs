caf364cd2631054545ce3756408fd15416920b87
Nathalia Silva <Silva.Nathalia@ibm.com>
2022-08-05 16:09:46 -0300

    Desafio

---
+++ Desafio05082022/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio05082022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            int funcionario = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salario = horas * valor;
+
+            Console.WriteLine("NUMBER = " + funcionario);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
